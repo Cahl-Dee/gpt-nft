56a8a1aad3cd3125c9896140bc02c947bc4650d6
Carl DiClementi <carldiclementi@gmail.com>
2023-07-12 13:33:10 -0500

    Update index.js
    testing something

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,7 @@ export default function Home() {
         <title>GPT NFT</title>
         <meta name="description" content="Ethereum wallet connection example with Next.js" />
         <link rel="icon" href="/favicon.ico" />
+    <script>(function () {Grin = window.Grin || (window.Grin = []);var s = document.createElement('script');s.type = 'text/javascript';s.async = true;s.src = 'https://d38xvr37kwwhcm.cloudfront.net/js/grin-sdk.js';var x = document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s, x);})();</script>
       </Head>
 
       <main>
